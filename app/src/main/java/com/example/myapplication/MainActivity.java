37d19b9e42ba9c79e223ecf5ff37d2d2458a4b9a
Manalalyami7 <manalalyami7@gmail.com>
2023-03-28 14:35:02 +0300

    lab2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b;
+EditText e;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        b = (Button) findViewById(R.id.send_button_id);
+e = findViewById(R.id.send_text_id);
+b.setOnClickListener(view -> {
+    String s= e.getText().toString();
+    Intent intent = new Intent(getApplicationContext(), second_activity.class);
+    intent.putExtra("PersonName",s);
+    startActivity(intent);
+});
     }
 }
